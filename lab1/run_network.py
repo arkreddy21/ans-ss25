10797bb9dae2ae87ac5dcb757c13164b598d0610
arkreddy <arkreddy2002@gmail.com>
2025-04-27 16:25:51 +0200

    lab1: added gateway addresses

--- lab1/run_network.py
+++ lab1/run_network.py
@@ -29,17 +29,18 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 
-class Router( Node ):
+class Router(Node):
     "A Node with IP forwarding enabled."
 
-    def config( self, **params ):
-        super( Router, self).config( **params )
+    def config(self, **params):
+        super(Router, self).config(**params)
         # Enable forwarding on the router
-        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+        self.cmd('sysctl net.ipv4.ip_forward=1')
 
-    def terminate( self ):
-        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
-        super( Router, self ).terminate()
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(Router, self).terminate()
+
 
 class NetworkTopo(Topo):
 
@@ -48,19 +49,26 @@ class NetworkTopo(Topo):
         Topo.__init__(self)
 
         # Build the specified network topology here
-        h1  = self.addHost('h1', ip="10.0.1.2/24")
-        h2  = self.addHost('h2', ip="10.0.1.3/24")
-        ext = self.addHost('ext', ip="192.168.1.123/24")
-        ser = self.addHost('ser', ip="10.0.2.2/24")
+        h1 = self.addHost('h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+        h2 = self.addHost('h2', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
+        ext = self.addHost('ext', ip='192.168.1.123/24', defaultRoute='via 192.168.1.1')
+        ser = self.addHost('ser', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        router = self.addNode('s3', cls=Router)
-        self.addLink(s1, h1, bw=15, delay="10ms")
-        self.addLink(s1, h2, bw=15, delay="10ms")
-        self.addLink(s2, ser, bw=15, delay="10ms")
-        self.addLink(s1, router, bw=15, delay="10ms")
-        self.addLink(s2, router, bw=15, delay="10ms")
-        self.addLink(ext, router, bw=15, delay="10ms")
+        router = self.addNode('s3', cls=Router, ip='10.0.1.1/24')
+        self.addLink(s1, h1, bw=15, delay='10ms')
+        self.addLink(s1, h2, bw=15, delay='10ms')
+        self.addLink(s2, ser, bw=15, delay='10ms')
+        self.addLink(s1, router, bw=15, delay='10ms',
+                     intfName2='s3-eth1', 
+                     params2={'ip': '10.0.1.1/24'})
+        self.addLink(s2, router, bw=15, delay='10ms',
+                     intfName2='s3-eth2', 
+                     params2={'ip': '10.0.2.1/24'})
+        self.addLink(ext, router, bw=15, delay='10ms',
+                     intfName2='s3-eth3', 
+                     params2={'ip': '192.168.1.1/24'})
+
 
 def run():
     topo = NetworkTopo()
@@ -69,13 +77,14 @@ def run():
                   link=TCLink,
                   controller=None)
     net.addController(
-        'c1', 
-        controller=RemoteController, 
-        ip="127.0.0.1", 
+        'c1',
+        controller=RemoteController,
+        ip='127.0.0.1',
         port=6653)
     net.start()
     CLI(net)
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel('info')
